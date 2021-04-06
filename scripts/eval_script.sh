dbae99047e7117507120a624ea5f1f2f51048bce
Shima Nofallah <shima@cs.uw.edu>
2021-04-06 14:52:15 -0700

    Update eval_script.sh

--- scripts/eval_script.sh
+++ scripts/eval_script.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+cd ..
+cd ./model
+
+
 echo "Stage_1"
 
 python3 validation.py \
 --txt_folder "/projects/patho1/sparse_segmentation/dataset/txt_files/stage_1/" \
---use_gpu True --gpu_id 3 \
 --classes "UL" "BG" "COR" "EP" "DE" \
 --loss_fcn "Dice" \
 --results "/projects/patho1/sparse_segmentation/results/stage_1/" \
@@ -16,7 +19,6 @@ echo "Stage_2_DE"
 
 python3 validation.py \
 --txt_folder "/projects/patho1/sparse_segmentation/dataset/txt_files/stage_2_DE/" \
---use_gpu True --gpu_id 3 \
 --classes "UL" "BG" "DMN" \
 --loss_fcn "Dice" \
 --results "/projects/patho1/sparse_segmentation/stuff/report/MIA/results/stage_2_DE/" \
@@ -28,7 +30,6 @@ echo "Stage_2_EP"
 
 python3 validation.py \
 --txt_folder "/projects/patho1/sparse_segmentation/dataset/txt_files/stage_2_EP/" \
---use_gpu True --gpu_id 3 \
 --classes "UL" "BG" "EPN" \
 --loss_fcn "Dice" \
 --results "/projects/patho1/sparse_segmentation/results/stage_2_EP_previous_93/" \
