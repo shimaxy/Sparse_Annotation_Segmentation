73428357e60ceb7341784c80820f5401c3dcabf0
Shima Nofallah <shima@cs.uw.edu>
2021-04-06 14:57:05 -0700

    Update resize.py

--- preprocessing/slide_segments/resize.py
+++ preprocessing/slide_segments/resize.py
@@ -1,8 +1,8 @@
 from PIL import Image
 from glob import glob 
-Image.MAX_IMAGE_PIXELS = 999999999
+Image.MAX_IMAGE_PIXELS = None
 
-path = '/projects/patho1/melanoma_diagnosis/mpath_x2.5/ROI_box'
+path = '/projects/patho1/melanoma_diagnosis/mpath_x2.5'
 
 for filename in sorted(glob(path + '/*.tif')):
     print(filename)
@@ -10,8 +10,7 @@ for filename in sorted(glob(path + '/*.tif')):
     w,h = image.size    
 
     image_r = image.resize((int(w/4), int(h/4)))
-    out_name = filename.replace('ROI_box','ROI_box_2.5')
-    out_name = out_name.replace('_x10_z0.tif','_x2.5_z0.jpg')
+    out_name = out_name.replace('_x10.tif','_x2.5.jpg')
     image_r.save(out_name)
     
     
